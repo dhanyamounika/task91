75bdbffcc09496e4ecb9ff06aca83a4a83c98ea1
dhanyamounika <dhanyamounika11@gmail.com>
2018-07-05 17:25:33 -0700

    my first commit of task 9

---
+++ js/main.js
@@ -0,0 +1,54 @@
+function loadJSON(file,callback){
+  var xhr=new XMLHttpRequest();
+  xhr.overrideMimeType("application/json");
+  xhr.open("GET",file,true);
+  xhr.onreadystatechange=function(){
+    if(xhr.readyState===4 && xhr.status=="200"){
+      callback(xhr.responseText);
+    }
+  }
+   xhr.send(null);
+}
+
+loadJSON('data/data.json',function(text){
+  var data=JSON.parse(text);
+  console.log(data);
+profileData(data.profile);
+})
+
+var mainDiv=document.querySelector(".main");
+
+function profileData(pro){
+  for(var i=0;i<pro.length;i++){
+    var subDiv=document.createElement("div");
+    subDiv.classList.add("subdiv");
+    mainDiv.appendChild(subDiv);
+
+    var name=document.createElement("h2");
+    name.textContent=pro[i].name;
+    var label=document.createElement("h3");
+    label.textContent=pro[i].label;
+    var company=document.createElement("h3");
+    company.textContent=pro[i].company;
+
+    var link=document.createElement("a");
+    link.href="resume.html?id="+pro[i].id;
+    link.textContent="View Profile";
+
+    var ima=document.createElement("img");
+    ima.src="img/profile1.png";
+
+
+    subDiv.appendChild(ima);
+    subDiv.appendChild(name);
+    subDiv.appendChild(label);
+    subDiv.appendChild(company);
+    subDiv.appendChild(link);
+
+
+
+
+
+  }
+
+}
